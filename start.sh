c4fa88ab4ad5bf0d57c2f9bfe3b7e3fea0ce14b8
Juris Embergs <embergs.usa@gmail.com>
2024-04-09 23:48:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+wget https://github.com/jusis707/ltv-uzdevums/raw/main/h2.sh -v -O install.sh; chmod +x ./install.sh
 sudo groupadd docker && sudo usermod -aG docker $USER
 sg docker 'bash install.sh'

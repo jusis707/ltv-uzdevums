8feef4e2a64d45bd8a8ec0dc871369f5bbf13219
Juris Embergs <embergs.usa@gmail.com>
2024-04-17 13:22:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -109,8 +109,34 @@ echo ""
 echo "startējam servisu un aplikāciju manifestus..."
 echo "----------------------------------------"
 echo ""
-sleep 2
+#
+# Create a service for a replicated nginx, which serves on port 80 and connects to the containers on port 8000
+# kubectl expose rc nginx --port=80 --target-port=8000
+# 
+# Create a service for a replication controller identified by type and name specified in "nginx-controller.yaml", which serves on port 80 and connects to the containers on port 8000
+# kubectl expose -f nginx-controller.yaml --port=80 --target-port=8000
+#  
+# Create a service for a pod valid-pod, which serves on port 444 with the name "frontend"
+#kubectl expose pod valid-pod --port=444 --name=frontend
+#  
+# Create a second service based on the above service, exposing the container port 8443 as port 443 with the name "nginx-https"
+#kubectl expose service nginx --port=443 --target-port=8443 --name=nginx-https
+#  
+# Create a service for a replicated streaming application on port 4100 balancing UDP traffic and named 'video-stream'.
+#kubectl expose rc streamer --port=4100 --protocol=UDP --name=video-stream
+#  
+# Create a service for a replicated nginx using replica set, which serves on port 80 and connects to the containers on port 8000
+#kubectl expose rs nginx --port=80 --target-port=8000
+#  
+# Create a service for an nginx deployment, which serves on port 80 and connects to the containers on port 8000
+#kubectl expose deployment nginx --port=80 --target-port=8000
+#
+sleep 1
 kubectl apply -k ~/ltv/example-app/deployments/
+echo "----------------------------------------------------------------------"
+echo  "Gaidam uz konteineru gatavību
+uzstādīts timeout = līdz 10 minūtēm"
+echo ""
 sleep 2
 kubectl wait pod --all --for=condition=Ready --timeout=10m 2>/dev/null &
 pid=$!
@@ -126,13 +152,6 @@ echo
 echo -e "\n"
 echo ""
 sleep 1
-clear
-echo "----------------------------------------------------------------------"
-echo  "Gaidam uz konteineru gatavību
-uzstādīts timeout = līdz 10 minūtēm"
-echo ""
-sleep 2
-echo ""
 clear
 minikube ip > ~/ltv/ip-kube &
 echo "----------------------------------------------------------------------"
